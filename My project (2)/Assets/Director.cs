f6226e11921681281e7163b2032cf9496d530b2a
Colorblind22 <36722245+Colorblind22@users.noreply.github.com>
2022-09-12 09:04:59 -0500

    attempting enemy spawning (not going well)

--- My project (2)/Assets/Director.cs
+++ My project (2)/Assets/Director.cs
@@ -1,23 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Director : MonoBehaviour
 {
     
-    public TextMesh waveText;
-    public int wave {get;set;}
-    public int enemyCount {get;set;}
+    public TMPro.TMP_Text waveText;
+    private int wave;
+    private int enemyCount;
+    private List<Transform> spawns;
+    public GameObject spawnPointContainer;
+    public GameObject enemyPrefab;
+    public Transform player;
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetWave(1);
+        GetComponentsInChildren<Transform>(false, spawns);
+        Debug.Log(spawns);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetWave(int arg)
+    {
+        this.wave = arg;
+        SetText();
+    }
+    public void SetEnemyCount(int arg)
+    {
+        this.enemyCount = arg;
+    }
+    public int GetWave() {return this.wave;}
+    public int GetEnemyCount() {return this.enemyCount;}
+    
+    void Spawn()
     {
         
+        for(int i = 0; i < this.enemyCount; i++)
+        {
+            
+        }
+    }
+    
+    void SetText()
+    {
+        if(waveText is not null) waveText.text = $"Wave {this.wave}";
     }
 }
